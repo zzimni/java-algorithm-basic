b604aa2f46f1d09300ce8e52f282ab3bba52d562
zzimni <hjm3327@naver.com>
2024-08-06 13:46:40 +0900

    패키지 구조 설정

--- src/section01_string/_01_문자_찾기/Main.java
+++ src/section01_string/_01_문자_찾기/Main.java
@@ -1,11 +1,24 @@
 package section01_string._01_문자_찾기;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
+public class Main {
+
+    public int solution(String str, char t) {
+        int answer = 0;
+        str = str.toUpperCase();
+        t=Character.toUpperCase(t);
+        for(char x : str.toCharArray()) {
+            if(x==t) answer++;
         }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+        char c = kb.next().charAt(0);
+        System.out.println(T.solution(str, c));
     }
 }
